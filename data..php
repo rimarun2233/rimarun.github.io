f4b954552fa6c2c43d0ce2fc7e6a2611a1504958
rimarun2233 <85119388+rimarun2233@users.noreply.github.com>
2021-06-27 18:31:43 +0900

    Create data..php

---
+++ data..php
@@ -0,0 +1,16 @@
+<?php 
+require_once("menu..php");
+
+$bread1 = new Menu("ネコ食パン", 180, "neko.images\pljhh.jpg");
+$bread2 = new Menu("クマ☆くりーむパン", 220, "neko.images\yukfry.jpg");
+$bread3 = new Menu("アンパンマンちょこパン", 250, "neko.images\iuui.jpg");
+$bread4 = new Menu("ネコ食パン♥ぺア", 340, "neko.images\uyk.jpg");
+
+$menus = array($bread1, $bread2, $bread3, $bread4,);
+
+
+
+
+
+
+?>

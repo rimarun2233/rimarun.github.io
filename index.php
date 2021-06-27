1fb7c5c6b97112b940956e273a7670fe45756347
rimarun2233 <85119388+rimarun2233@users.noreply.github.com>
2021-06-27 18:18:51 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ネコのパン屋さんホーム</title>
+  <link rel="stylesheet" type="text/css" href="neko.css">
+  <link rel="stylesheeet" href="responsive.css">
+</head>
+<body>
+  <div class="header">
+    <div class="header-logo">ネコのパン屋さん</div>
+    <div class="header-right">
+      <div class="header-list">
+        <ul>
+          <li>
+            <a href="index.php" class="btn home">ホーム</a>
+          </li>
+          <li>
+          <a href="" class="btn company">会社概要</a>
+          </li>
+          <li>
+            <a href="contactUs.php" class="btn contact">お問い合わせ</a>
+          </li>
+          <li>
+          <a href="menu_page.php" class="btn product">商品紹介</a>
+          </li>
+        </ul>
+    </div>
+    </div>
+  </div>
+  
+  <div class="main-wrapper">
+    <img src="neko.images\hyhuu.jpg" class="neko">
+    
+  </div>
+  
+</body>
